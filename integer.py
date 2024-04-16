98f93ad18dc7361c08b41bdbaf49d3673b6583be
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-17 00:50:37 +0700

    Add integer.py

---
+++ integer.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+  dec = 10
+  print(f"dec: {dec}")
+  print(f"type(dec): {type(dec)}")
+
+  bin = 0b10
+  print(f"bin: {bin}")
+  print(f"type(bin): {type(bin)}")
+
+  oct = 0o10
+  print(f"oct: {oct}")
+  print(f"type(oct): {type(oct)}")
+
+  hex = 0x10
+  print(f"hex: {hex}")
+  print(f"type(hex): {type(hex)}")
